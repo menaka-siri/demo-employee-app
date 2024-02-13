4645f5ed209b1776f19579d5a509c52e60a47c15
Menaka S <menaka442@gmail.com>
2024-02-13 07:54:34 +0530

    add crud to EmployeeService

--- src/main/java/com/zealtrack/employeecrud/service/EmployeeServiceImpl.java
+++ src/main/java/com/zealtrack/employeecrud/service/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.zealtrack.employeecrud.service;
 import com.zealtrack.employeecrud.dao.EmployeeDAO;
 import com.zealtrack.employeecrud.dao.EmployeeDAOImpl;
 import com.zealtrack.employeecrud.entity.Employee;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,5 +22,22 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        return employeeDAO.findById(theId);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee theEmployee) {
+        return employeeDAO.save(theEmployee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(int theId) {
+        employeeDAO.deleteById(theId);
     }
 }
