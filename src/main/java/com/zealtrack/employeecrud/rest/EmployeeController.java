955a827e4ce35e19b5aa516f770365909cc9c46c
Menaka S <menaka442@gmail.com>
2024-02-13 08:13:04 +0530

    add crud to EmployeeController

--- src/main/java/com/zealtrack/employeecrud/rest/EmployeeController.java
+++ src/main/java/com/zealtrack/employeecrud/rest/EmployeeController.java
@@ -4,6 +4,7 @@ import com.zealtrack.employeecrud.dao.EmployeeDAO;
 import com.zealtrack.employeecrud.entity.Employee;
 import com.zealtrack.employeecrud.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,16 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getEmployees() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee == null) {
+            throw  new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return theEmployee;
     }
 }
