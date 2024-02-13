f477b2c3a17e00b8233e8c33433f401fc2822b7f
Menaka S <menaka442@gmail.com>
2024-02-13 07:47:47 +0530

    add crud to EmployeeDAOImpl

--- src/main/java/com/zealtrack/employeecrud/dao/EmployeeDAOImpl.java
+++ src/main/java/com/zealtrack/employeecrud/dao/EmployeeDAOImpl.java
@@ -23,4 +23,22 @@ public class EmployeeDAOImpl implements EmployeeDAO {
         List<Employee> employees = theQuery.getResultList();
         return employees;
     }
+
+    @Override
+    public Employee findById(int theId) {
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+        return theEmployee;
+    }
+
+    @Override
+    public Employee save(Employee theEmployee) {
+        Employee dbEmployee = entityManager.merge(theEmployee); //if id == 0, then insert, else update
+        return dbEmployee;
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+        entityManager.remove(theEmployee);
+    }
 }
